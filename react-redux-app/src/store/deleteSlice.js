11581ee5286b3f59bcb06b95e35384292fe17581
IanX98 <ianpedro7@gmail.com>
2025-05-02 00:41:01 -0300

    delete refactor

--- react-redux-app/src/store/deleteSlice.js
+++ react-redux-app/src/store/deleteSlice.js
@@ -1,5 +1,4 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { faker } from "@faker-js/faker";
 
 export const loadDeleteDataset = createAsyncThunk(
   "delete/loadDataset",
@@ -10,16 +9,6 @@ export const loadDeleteDataset = createAsyncThunk(
   }
 );
 
-const generateData = (count) => {
-  return Array.from({ length: count * 2 }, (_, i) => ({
-    id: i + 1,
-    model: faker.vehicle.model(),
-    plate: faker.vehicle.vin(),
-    capacity: faker.number.int({ min: 2, max: 7 }),
-    lastMaintenance: faker.date.past().toLocaleDateString(),
-  }));
-};
-
 const initialState = {
   items: [],
   deleteTime: null,
@@ -29,10 +18,6 @@ const deleteSlice = createSlice({
   name: "delete",
   initialState,
   reducers: {
-    generateData(state, action) {
-      const count = action.payload;
-      state.items = generateData(count);
-    },
     deleteRows(state, action) {
       const count = action.payload;
       const start = performance.now();
